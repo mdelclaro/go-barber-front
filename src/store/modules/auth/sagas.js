0fab5d8872ce3a5b18fb35df1e53467ad140e433
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-08-25 20:53:52 -0300

    added toast

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -17,7 +18,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Not a provider');
+      toast.error('Not a provider');
       return;
     }
 
@@ -25,6 +26,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Login failed. Check your credentials');
     yield put(signFailure());
   }
 }
