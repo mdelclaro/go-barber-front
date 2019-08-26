34bd95d5af41c2a7b3f604bd09c58a4645ed7742
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-08-25 21:02:19 -0300

    sign up

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
@@ -16,7 +19,12 @@ const schema = Yup.object().shape({
 });
 
 export default function SignUp() {
-  function handleSubmit(data) {}
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <>
@@ -27,7 +35,9 @@ export default function SignUp() {
         <Input name="email" type="email" placeholder="Email" />
         <Input name="password" type="password" placeholder="Password" />
 
-        <button type="submit">Create account</button>
+        <button type="submit">
+          {loading ? 'Loading...' : 'Create account'}
+        </button>
         <Link to="/">I already have an account</Link>
       </Form>
     </>
