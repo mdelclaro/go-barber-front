5ef8cbbf66b2f678134230e0479a0ddbd5d6da2e
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-08-25 21:25:19 -0300

    header styling

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+
+import header from '~/assets/header.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={header} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>Test User</strong>
+              <Link to="/profile">My profile</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Test User"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
