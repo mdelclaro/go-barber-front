874b69fe9982005c2c48929cf4dfd94324aedbef
Matheus Del Claro <matheusdelclaro@gmail.com>
2019-08-25 22:51:29 -0300

    profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Your name" />
+        <Input name="email" type="email" placeholder="Email address" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Old password" />
+        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm password"
+        />
+
+        <button type="submit">Update profile</button>
+      </Form>
+
+      <button type="button">Sign out</button>
+    </Container>
+  );
 }
